bad6cfd9b0468c13fed1e4f632fe09ac09c41908
snitochka <107924262+snitochka@users.noreply.github.com>
2025-06-18 21:19:09 +0300

    Create tasks-01.js

---
+++ js-tasks/tasks-01.js
@@ -0,0 +1,22 @@
+// 1. Messi Goals 
+// https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+
+// -- Snitko Anna --
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  // code example
+}
+
+// 2. Make negative
+// https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+
+// 3. Game Move
+// https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+
+// 4. Personalized Message
+// https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+
+// 5. Keep Hydrated
+// https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+
+// 6. Opposites Attract
+//https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
