61acd2853c16c3b29196ca78acdd2011c3744deb
figinspector <figinspector@gmail.com>
2025-06-18 22:53:21 +0300

    feat: add solutions for task#7

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -1,22 +1,52 @@
-// 1. Messi Goals 
+// 1. Messi Goals
 // https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
 
 // -- Snitko Anna --
-function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   // code example
+}
+
+// -- Valentyn Tymofiiv --
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
 
 // 2. Make negative
 // https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 
+// -- Valentyn Tymofiiv --
+function makeNegative(num) {
+  return num > 0 ? -num : num;
+}
+
 // 3. Game Move
 // https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+
+// -- Valentyn Tymofiiv --
+function move(position, roll) {
+  return position + roll * 2;
+}
 
 // 4. Personalized Message
 // https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
 
+// -- Valentyn Tymofiiv --
+function greet(name, owner) {
+  return name === owner ? "Hello boss" : "Hello guest";
+}
+
 // 5. Keep Hydrated
 // https://www.codewars.com/kata/keep-hydrated-1/train/javascript
 
+// -- Valentyn Tymofiiv --
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
+
 // 6. Opposites Attract
 //https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+
+// -- Valentyn Tymofiiv --
+function lovefunc(flower1, flower2) {
+  return (flower1 + flower2) % 2 === 1;
+}
