a0817cd5340198a4e1385ed58268677dd0d274d1
Andrii Rybak <112274930+1-Gilgamesh@users.noreply.github.com>
2025-06-19 22:14:48 +0300

    Update tasks-02.js

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -9,6 +9,11 @@ function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
 // -- Snitko Anna --
 const goals = (laLigaGoals, copaDelReyGoals, championsLeagueGoals) => laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 
+// === Andrii Rybak ===
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+} 
+
 // 2. Make negative
 // https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 
@@ -19,6 +24,11 @@ function makeNegative(num) {
 
 // -- Snitko Anna --
 const makeNegative = num => num > 0 ? -num : num;
+
+// === Andrii Rybak ===
+function makeNegative(num) {
+  return num > 0 ? -num : num;
+}
 
 // 3. Game Move
 // https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
@@ -31,6 +41,11 @@ function move(position, roll) {
 // -- Snitko Anna --
 const move = (position, roll) => position + roll * 2;
 
+// === Andrii Rybak ===
+function move (position, roll) {
+  return position + 2 * roll;
+}
+
 // 4. Personalized Message
 // https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
 
@@ -41,6 +56,11 @@ function greet(name, owner) {
 
 // -- Snitko Anna --
 const greet = (name, owner) => name === owner ? "Hello boss" : "Hello guest";
+
+// === Andrii Rybak ===
+function greet (name, owner) {
+  return name === owner ? "Hello boss" : "Hello guest";
+}
 
 // 5. Keep Hydrated
 // https://www.codewars.com/kata/keep-hydrated-1/train/javascript
@@ -53,6 +73,11 @@ function litres(time) {
 // -- Snitko Anna --
 const litres = (time) => Math.floor(time * 0.5);
 
+// === Andrii Rybak ===
+function litres(time) {
+  return Math.floor(0.5 * time);
+}
+
 // 6. Opposites Attract
 //https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
 
@@ -63,3 +88,8 @@ function lovefunc(flower1, flower2) {
 
 // -- Snitko Anna --
 const lovefunc = (flower1, flower2) => (flower1 + flower2) % 2 === 1;
+
+// === Andrii Rybak ===
+function lovefunc(flower1, flower2){
+  return (flower1 + flower2) % 2 === 1;
+}
