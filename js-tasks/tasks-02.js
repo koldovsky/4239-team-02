b112fffef12273a946efeaf7b6da1e596622f3d2
snitochka <107924262+snitochka@users.noreply.github.com>
2025-06-19 09:00:10 +0300

    Update tasks-02.js

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -1,15 +1,13 @@
 // 1. Messi Goals
 // https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
 
-// -- Snitko Anna --
-function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
-  // code example
-}
-
 // -- Valentyn Tymofiiv --
 function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
+
+// -- Snitko Anna --
+const goals = (laLigaGoals, copaDelReyGoals, championsLeagueGoals) => laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 
 // 2. Make negative
 // https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
@@ -19,6 +17,9 @@ function makeNegative(num) {
   return num > 0 ? -num : num;
 }
 
+// -- Snitko Anna --
+const makeNegative = num => num > 0 ? -num : num;
+
 // 3. Game Move
 // https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
 
@@ -26,6 +27,9 @@ function makeNegative(num) {
 function move(position, roll) {
   return position + roll * 2;
 }
+
+// -- Snitko Anna --
+const move = (position, roll) => position + roll * 2;
 
 // 4. Personalized Message
 // https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
@@ -35,6 +39,9 @@ function greet(name, owner) {
   return name === owner ? "Hello boss" : "Hello guest";
 }
 
+// -- Snitko Anna --
+const greet = (name, owner) => name === owner ? "Hello boss" : "Hello guest";
+
 // 5. Keep Hydrated
 // https://www.codewars.com/kata/keep-hydrated-1/train/javascript
 
@@ -43,6 +50,9 @@ function litres(time) {
   return Math.floor(time * 0.5);
 }
 
+// -- Snitko Anna --
+const litres = (time) => Math.floor(time * 0.5);
+
 // 6. Opposites Attract
 //https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
 
@@ -50,3 +60,6 @@ function litres(time) {
 function lovefunc(flower1, flower2) {
   return (flower1 + flower2) % 2 === 1;
 }
+
+// -- Snitko Anna --
+const lovefunc = (f1, f2) => (f1 + f2) % 2 === 1;
