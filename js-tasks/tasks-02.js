7fb6988a55afab8c54d8bf68be11e9cbb572d109
snitochka <107924262+snitochka@users.noreply.github.com>
2025-06-20 21:31:40 +0300

    Create tasks-02.js

---
+++ js-tasks/tasks-02.js
@@ -0,0 +1,29 @@
+// ---БАЗОВІ ЗАДАЧІ---
+// 1. https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+
+// -- Anna Snitko --
+function stringToArray(string){
+	// code code code
+}
+
+// 2. https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+
+
+// ---ПОГЛИБЛЕНІ ЗАДАЧІ---
+// 1. https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+
+// 2. https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
+
+
+// ---ДОДАТКОВІ ЗАДАЧІ---
+// 1. https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
+
+// 2. https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript
+
+// 3. https://www.codewars.com/kata/5933a1f8552bc2750a0000ed/train/javascript
+
+// 4. https://www.codewars.com/kata/574b3b1599d8f897470018f6/train/javascript
+
+// 5. https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/javascript
+
+// 6. https://www.codewars.com/kata/5545f109004975ea66000086/train/javascript
