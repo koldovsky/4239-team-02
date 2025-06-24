fe5a67051709a621c5805ecfcc73b91cd6706a5f
snitochka <107924262+snitochka@users.noreply.github.com>
2025-06-24 19:20:19 +0000

    Header and header-hero changes

---
+++ js/global.header-burger.js
@@ -0,0 +1,24 @@
+const burger = document.querySelector(".header__burger");
+const nav = document.querySelector(".header-nav__panel-mobile");
+const menuLinks = document.querySelectorAll(".header__menu-link");
+const header = document.querySelector(".header");
+
+function toggleMenu() {
+  burger.classList.toggle("is-active");
+  nav.classList.toggle("is-open");
+  document.body.classList.toggle("no-scroll");
+  header.classList.toggle("no-scroll");
+}
+
+function closeMenu() {
+  burger.classList.remove("is-active");
+  nav.classList.remove("is-open");
+  document.body.classList.remove("no-scroll");
+  header.classList.remove("no-scroll");
+}
+
+burger.addEventListener("click", toggleMenu);
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", closeMenu);
+});
