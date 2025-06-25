0bc1aad6b0938e47d27637bab43f9821b24a1faf
snitochka <107924262+snitochka@users.noreply.github.com>
2025-06-25 14:32:47 +0300

    Update tasks-03.js

--- js-tasks/tasks-03.js
+++ js-tasks/tasks-03.js
@@ -1,16 +1,11 @@
 // https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
 
-// -- Anna Snitko --
-function circleCircumference(circle) {}
-
-// -- Valentyn Tymofiiv --
 function circleCircumference(circle) {
   return 2 * Math.PI * circle.radius
 }
 
 // https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
 
-// -- Valentyn Tymofiiv --
 function giveMeFive(obj) {
   const res = [];
   for (const key in obj) {
@@ -19,46 +14,23 @@ function giveMeFive(obj) {
   }
 
   return res;
-const circleCircumference = circle => 2 * Math.PI * circle.radius;
-
-// https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
-
-// -- Anna Snitko --
-function giveMeFive(obj) {
-  var five = [];
-  for (var key in obj) {
-    if (key.length === 5) five.push(key);
-    if (obj[key].length === 5) five.push(obj[key]);
-  }
-  return five;
 }
 
 // https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
 
-// -- Valentyn Tymofiiv --
 function buildFun(n) {
   const res = [];
   
   for (let i = 0; i < n; i++) res.push(() => i);
   
   return res;
-// -- Anna Snitko --
-function buildFun(n){
-	let res = [];
-	for (let i = 0; i < n; i++) res.push(() => i);
-	return res;
 }
 
 // https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
 
-// -- Valentyn Tymofiiv --
 class Shark extends Animal {
   constructor(name, age, status) {
     super(name, age, 0, 'shark', status);
-// -- Anna Snitko --
-class Shark extends Animal {
-  constructor(name, age, status) {
-    super(name, age, 0, "shark", status);
   }
 }
 
@@ -69,10 +41,6 @@ class Cat extends Animal {
   
   introduce() {
     return super.introduce() + "  Meow meow!";
-    super(name, age, 4, "cat", status);
-  }
-  introduce() {
-    return `${super.introduce()}  Meow meow!`;
   }
 }
 
@@ -84,12 +52,5 @@ class Dog extends Animal {
   
   greetMaster() {
     return `Hello ${this.master}`
-  }
-}
-    super(name, age, 4, "dog", status);
-    this.master = master;
-  }
-  greetMaster() {
-    return `Hello ${this.master}`;
   }
 }
