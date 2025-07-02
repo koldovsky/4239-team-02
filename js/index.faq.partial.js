65f5814957e4b9196f630c4338e300281a521f7c
Oleksandr Ryzhkov <ryzhkov221984@gmail.com>
2025-07-02 13:03:31 +0000

    rebuild faq part

--- js/index.faq.partial.js
+++ js/index.faq.partial.js
@@ -1,6 +1,13 @@
 document.querySelectorAll('.faq__question').forEach(button => {
-    button.addEventListener('click', () => {
-      const item = button.closest('.faq__description');
-      item.classList.toggle('faq__item--active');
+  button.addEventListener('click', () => {
+    const item = button.closest('.faq__item');
+
+    // Закрити інші акордеони
+    document.querySelectorAll('.faq__item').forEach(i => {
+      if (i !== item) i.classList.remove('faq__item--active');
     });
+
+    // Перемкнути поточний
+    item.classList.toggle('faq__item--active');
   });
+});
