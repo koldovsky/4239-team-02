8673d98efcf8df4b03b05d5d97fa906cacaeb007
i-taras <110666534+i-taras@users.noreply.github.com>
2025-07-03 15:26:11 +0000

    add blog details

---
+++ data/blog-post-grapes.js
@@ -0,0 +1,20 @@
+export default {
+  title: "Grapes for Winemaking",
+  date: "1 Feb 2022",
+  image: "img/blog/grapes-big.png",
+  content: `
+    <p class="blog-post__paragraph">
+      The most important thing in wine production is the quality of the grapes. Without this, it is impossible to make good wine. 
+      Therefore, there are a number of requirements for growing grapes to meet the requirements for winemaking.
+    </p>
+    <p class="blog-post__paragraph">
+      Vintage wine cannot be made from grapes without prior selection, low-quality berries can ruin the whole taste. 
+      Special grape varieties are used for winemaking. They can be either red or white, depending on what kind of wine they plan to make from it. 
+      The grapes must be fully ripe, and they must be cut off strictly at a certain time so that they do not overripe.
+    </p>
+    <p class="blog-post__paragraph">
+      Not all berries are suitable for making wine. For vintage wines, not only bunches are selected but also each berry. 
+      The berry should be large and fleshy. It must not be damaged in any way. Unripe or overripe berries are unacceptable.
+    </p>
+  `
+};
---
+++ data/blog-post-wine-rated.js
@@ -0,0 +1,19 @@
+export default {
+  title: "How Is Wine Rated?",
+  date: "5 Feb 2022",
+  image: "img/blog/wine-rated-big.png",
+  content: `
+    <p class="blog-post__paragraph">
+      Awards are the most important aspect for wines. 
+      If experts rate a particular wine highly, then it indicates that the production technology has been fully followed, and it is a really high-quality product. 
+      The award-winning wine is highly regarded by consumers as well.
+    </p>
+    <p class="blog-post__paragraph">
+      Therefore, producers regularly exhibit their wines at various competitions. 
+      The higher the status of the event, the tougher the competition, and, consequently, the more prestigious the award. The assessment is carried out by independent experts. 
+      During the assessment, the specialist evaluates the color of the product, its aroma, and structure. The last stage of the assessment is the assessment of taste. 
+      For each aspect, the wine receives a certain grade. 
+      The maximum score in the United States is 100 points.
+    </p>
+  `
+};
