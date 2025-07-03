2bd8a64b666a7b6c2e10fcb6f128dd82ffab70b0
i-taras <110666534+i-taras@users.noreply.github.com>
2025-07-03 15:32:17 +0000

    add blog js

---
+++ js/blog-post.js
@@ -0,0 +1,13 @@
+const postSlug = new URLSearchParams(window.location.search).get("post");
+
+console.log("POST SLUG =", postSlug); // DEBUG
+
+import(`../data/blog-post-${postSlug}.js`).then((module) => {
+  const data = module.default;
+
+  document.getElementById("blog-image").src = data.image;
+  document.getElementById("blog-image").alt = data.title;
+  document.getElementById("blog-date").textContent = data.date;
+  document.getElementById("blog-title").textContent = data.title;
+  document.getElementById("blog-content").innerHTML = data.content;
+});
